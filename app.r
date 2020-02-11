9c7cf4255febafa8131778f73d21b5291cdf843d
Tyler Cobian <tylercobian@169-231-150-167.wireless.ucsb.edu>
2020-02-11 14:08:31 -0800

    Create the set up for the app

---
+++ app.r
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+
+
+
+
+ui<- dashboardPage()
+
+
+
+
+
+
+
+
+
+
+
+
+server<- function(input, output){}
+
+
+
+
+
+shinyApp(ui = ui, server = server)
+
+
+
+
