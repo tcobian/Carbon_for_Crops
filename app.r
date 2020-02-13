437db2f3b26a36b760b23b2146de19646b1f633a
Tyler Cobian <tylercobian@Tylers-MacBook-Air-4.local>
2020-02-13 11:49:29 -0800

    Put points on the map of where crops are grown

--- app.r
+++ app.r
@@ -35,7 +35,26 @@ ui<- dashboardPage(skin = "black",
 
 server<- function(input, output){
   
-  output$map_1<- renderTmap(tm_basemap("CartoDB.DarkMatter"))
+  map_data<- tribble(
+    ~id, ~lon, ~lat,
+    "Cotton", -95.300003, 32.349998,
+    "Cotton", 87.5396, 42.5246,
+    "Cotton", 78.6569, 22.9734,
+    "Cotton", -80.6549, -5.1783,
+    "Bison Grazing", -103.2310, 44.0805,
+    "Bison Grazing", -56.9211, -12.6819,
+    "Mango", 72.8777, 19.0760,
+    "Mango", -85.2072, 12.8654,
+    "Kernza", -94.6859, 46.7296,
+    "Kernza", -98.4842, 39.0119
+  )
+
+  map_data_sf<- st_as_sf(map_data, coords = c("lon", "lat"))
+  
+  
+  output$map_1<- renderTmap(tm_basemap("CartoDB.DarkMatter")+
+                              tm_shape(map_data_sf)+
+                              tm_dots(label = "id", col = "id", size = 0.1))
   
 }
   
