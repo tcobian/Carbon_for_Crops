535cb95f6ba62d92700044409a5c886cb39a23ac
Tyler Cobian <tylercobian@Tylers-MacBook-Air-4.local>
2020-02-13 10:30:58 -0800

    Trying to bring up tmap

--- app.r
+++ app.r
@@ -2,24 +2,51 @@ library(tidyverse)
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
+library(tmap)
+library(sf)
 
 
 
 
-ui<- dashboardPage()
+ui<- dashboardPage(skin = "black",
+  dashboardHeader(title = "Carbon for Crops"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Regenratives Globally", tabName = "map"),
+      menuItem("Soil Organic Carbon & GWP", tabName = "overview"),
+      menuItem("Practices", tabName = "sensativity"),
+      menuItem("Sources of GHG's", tabName = "ghg")
+  )),
+  dashboardBody(
+    tabItem(
+      tabItem(
+        tabName = "map",
+        fluidPage(
+          box(
+            title = "Potential for Regenratives Within Supply Chain",
+            tmapOutput(outputId = "map")
+          )
+        )
+      )
+    )
+  )
+)
 
 
 
 
 
+server<- function(input, output){
+  
+  output$map<- renderTmap({
+    tm_basemap("CartoDB.DarkMatter")
+    tmap_mode("view")
+    })
+  
+}
 
 
 
-
-
-
-
-server<- function(input, output){}
 
 
 
