363cef79433de2da657530805484fab6149799b5
Tyler Cobian <tylercobian@Tylers-MacBook-Air-4.local>
2020-02-24 08:58:50 -0800

    Updates to the map

--- app.r
+++ app.r
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(kableExtra)
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
@@ -6,6 +7,105 @@ library(tmap)
 library(sf)
 library(leaflet)
 
+
+crops<- read_csv("Total_Crops.csv")
+
+####################################################
+# map markers
+####################################################
+
+###### Cotton
+# TEXAS
+cotton_regen_texas_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "Texas") %>%
+  filter(Practice == "Regenerativeerative") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_organic_texas_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "Texas") %>%
+  filter(Practice == "Organic") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_texas<- bind_rows(cotton_regen_texas_table, cotton_organic_texas_table)
+colnames(cotton_texas)<- c("Yearly Change kgSOC", "Average Net GHG kgCO2e")
+rownames(cotton_texas)<- c("Regenerative ", "Organic")
+
+cotton_texas_table<- kable(cotton_texas, caption = "Cotton: Texas") %>% 
+  kable_styling(bootstrap_options = "striped")
+cotton_texas_table
+
+#INDIA
+cotton_regen_india_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "India") %>%
+  filter(Practice == "Regenerativeerative") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_organic_india_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "India") %>%
+  filter(Practice == "Organic") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_india<- bind_rows(cotton_regen_india_table, cotton_organic_india_table)
+colnames(cotton_india)<- c("Yearly Change kgSOC", "Average Net GHG kgCO2e")
+rownames(cotton_india)<- c("Regenerative ", "Organic")
+
+cotton_india_table<- kable(cotton_india, caption = "Cotton: India") %>% 
+  kable_styling(bootstrap_options = "striped")
+cotton_india_table
+
+# CHINA
+cotton_regen_china_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "China") %>%
+  filter(Practice == "Regenerativeerative") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_organic_china_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "China") %>%
+  filter(Practice == "Organic") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_china<- bind_rows(cotton_regen_china_table, cotton_organic_china_table)
+colnames(cotton_china)<- c("Yearly Change kgSOC", "Average Net GHG kgCO2e")
+rownames(cotton_china)<- c("Regenerative ", "Organic")
+
+cotton_china_table<- kable(cotton_china, caption = "Cotton: China") %>% 
+  kable_styling(bootstrap_options = "striped")
+cotton_china_table
+
+#PERU
+cotton_regen_peru_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "Peru") %>%
+  filter(Practice == "Regenerativeerative") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_organic_peru_table<- crops %>% 
+  filter(Crop == "Cotton") %>% 
+  filter(Country == "Peru") %>%
+  filter(Practice == "Organic") %>% 
+  summarise(mean_regen_soc = mean(dSOC),
+            mean_regen_gwp = mean(GWP))
+
+cotton_peru<- bind_rows(cotton_regen_peru_table, cotton_organic_peru_table)
+colnames(cotton_peru)<- c("Yearly Change kgSOC", "Average Net GHG kgCO2e")
+rownames(cotton_peru)<- c("Regenerative ", "Organic")
+
+cotton_peru_table<- kable(cotton_peru, caption = "Cotton: Peru") %>% 
+  kable_styling(bootstrap_options = "striped")
+cotton_peru_table
 
 
 
@@ -55,7 +155,30 @@ server<- function(input, output){
   
   output$map_1<- renderLeaflet(leaflet(map_data) %>% 
                                  addProviderTiles("CartoDB.DarkMatter") %>% 
-                                 addMarkers(lng = ~lon, lat = ~lat, popup = ~id) %>% 
+                                 addCircleMarkers(lng = -95.300003, 
+                                                  lat = 32.349998, 
+                                                  popup = cotton_texas_table, 
+                                                  stroke = FALSE, 
+                                                  fillColor = "red", 
+                                                  fillOpacity = 0.3) %>% 
+                                 addCircleMarkers(lng = 78.6569, 
+                                                  lat = 22.9734, 
+                                                  popup = cotton_india_table, 
+                                                  stroke = FALSE, 
+                                                  fillColor = "red", 
+                                                  fillOpacity = 0.3) %>% 
+                                 addCircleMarkers(lng = 87.5396, 
+                                                  lat = 42.5246, 
+                                                  popup = cotton_china_table, 
+                                                  stroke = FALSE, 
+                                                  fillColor = "red", 
+                                                  fillOpacity = 0.3) %>% 
+                                 addCircleMarkers(lng = -80.6549, 
+                                                  lat = -5.1783, 
+                                                  popup = cotton_peru_table, 
+                                                  stroke = FALSE, 
+                                                  fillColor = "red", 
+                                                  fillOpacity = 0.3) %>% 
                                  setView(40, 6, 2))  
 
   
